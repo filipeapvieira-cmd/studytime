82e031014874d97ce8524c9e018a4dc28f3f33a6
FILIPE VIEIRA <no@no.com>
2023-06-15 20:53:41 +0200

    refactor image upload component

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -5,23 +5,16 @@ import React from "react";
 import ImageUploadItem from "./ImageUploadItem";
 import { useToast } from "@/components/ui/use-toast";
 import { UploadImagesContext } from "@/src/ctx/upload-images-provider";
+import { useValidation } from "@/lib/imgUploadUtils";
 
 interface ImageUploadProps {}
 
 const ImageUpload: FC<ImageUploadProps> = ({}) => {
   const { validFile, setValidFile } = useContext(UploadImagesContext);
   const [isHovered, setIsHovered] = useState(false);
-  const { toast } = useToast();
+  const { isValid } = useValidation(validFile);
 
   const inputFileRef = React.createRef<HTMLInputElement>();
-
-  const showToast = (description: string) => {
-    toast({
-      variant: "destructive",
-      title: `Uh oh! Something went wrong`,
-      description,
-    });
-  };
 
   const onClickHandler = () => {
     inputFileRef.current?.click();
@@ -47,60 +40,12 @@ const ImageUpload: FC<ImageUploadProps> = ({}) => {
     onFileHandler(e.dataTransfer.files);
   };
 
-  const extensionValidation = (file: File) => {
-    const validTypes = [
-      "image/jpeg",
-      "image/jpg",
-      "image/png",
-      "image/gif",
-      "image/x-icon",
-      "image/bmp",
-      "image/webp",
-      "image/svg+xml",
-    ];
-
-    if (!(file.type && validTypes.includes(file.type))) {
-      showToast("Invalid file type!");
-      return false;
-    }
-    return true;
-  };
-
-  const sizeValidation = (fileSize: number) => {
-    if (fileSize === 0 || fileSize > 30000000) {
-      showToast(
-        `${
-          fileSize === 0 ? "File size is 0" : "File size is greater than 30MB"
-        }`
-      );
-      return false;
-    }
-    return true;
-  };
-
-  const duplicateNameValidation = (fileName: String) => {
-    const isDuplicate = validFile.find((file) => file.name === fileName);
-    if (isDuplicate) {
-      showToast("Duplicate file name!");
-      return false;
-    }
-    return true;
-  };
-
-  const fileValidation = (file: File) => {
-    const isValid =
-      extensionValidation(file) &&
-      sizeValidation(file.size) &&
-      duplicateNameValidation(file.name);
-    return isValid;
-  };
-
   const onFileHandler = (files: FileList | null = null) => {
     if (!files) {
       return;
     }
     for (let i = 0; i < files.length; i++) {
-      if (fileValidation(files[i])) {
+      if (isValid(files[i])) {
         setValidFile((prev) => [...prev, files[i]]);
       }
     }
---
+++ lib/imgUploadUtils.ts
@@ -0,0 +1,60 @@
+import { useToast } from "@/components/ui/use-toast";
+
+export const useValidation = (validFile: File[]) => {
+    const { toast } = useToast();
+
+    const showToast = (description: string) => {
+        toast({
+          variant: "destructive",
+          title: `Uh oh! Something went wrong`,
+          description,
+        });
+      };
+
+      const extensionValidation = (file: File) => {
+        const validTypes = [
+          "image/jpeg",
+          "image/jpg",
+          "image/png",
+          "image/gif",
+          "image/x-icon",
+          "image/bmp",
+          "image/webp",
+          "image/svg+xml",
+        ];
+    
+        if (!(file.type && validTypes.includes(file.type))) {
+          showToast("Invalid file type!");
+          return false;
+        }
+        return true;
+      };
+
+      const sizeValidation = (fileSize: number) => {
+        if (fileSize === 0 || fileSize > 30000000) {
+          showToast(
+            `${
+              fileSize === 0 ? "File size is 0" : "File size is greater than 30MB"
+            }`
+          );
+          return false;
+        }
+        return true;
+      };
+
+      const duplicateNameValidation = (fileName: String) => {
+        const isDuplicate = validFile.find((file) => file.name === fileName);
+        if (isDuplicate) {
+          showToast("Duplicate file name!");
+          return false;
+        }
+        return true;
+      };
+
+      const isValid = (file: File) => extensionValidation(file) && sizeValidation(file.size) && duplicateNameValidation(file.name);
+
+      return { isValid };
+    }
+    
+
+
