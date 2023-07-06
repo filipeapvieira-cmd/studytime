c830d776f125c9136363419cba05003bb2f15333
FILIPE VIEIRA <no@no.com>
2023-07-06 22:07:57 +0200

    deleted legacy code

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,28 +1,19 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import alerts from "@/text/alerts.json"
-import { ControlText, SessionTimer } from "@/types"
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import alerts from "@/text/alerts.json";
+import { ControlText, SessionTimer } from "@/types";
 import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const retrieveTextFromJson = (keyName: ControlText["action"]): { title: string, description: string } => {
+export const retrieveTextFromJson = (
+  keyName: ControlText["action"]
+): { title: string; description: string } => {
   const text = alerts[keyName];
   return { title: text.title, description: text.description };
-}
-
-export const calcSessionTimes = (sessionTimes: SessionTimer) => {
-  const { effectiveTimeOfStudy, sessionStartTime } = sessionTimes;
-  const sessionEndTime = Date.now();
-  const totalElapsedTime = sessionEndTime - sessionStartTime;
-  const totalPauseTime = totalElapsedTime - effectiveTimeOfStudy * 1000; // effectiveTimeOfStudy is calculated in seconds, so here needs to be converted to ms
-
-  console.log("Total elapsed time: ", totalElapsedTime);
-
-  return { sessionEndTime, totalPauseTime };
-}
+};
 
 export const getFileSize = (fileSize: number) => {
   const bytesInAKilobyte = 1024;
@@ -39,10 +30,15 @@ interface uploadFileProps {
   file: File;
   setUploadProgress: (progress: number) => void;
   setImgUrls: (urls: { imgUrl: string; deleteUrl: string }) => void;
-  setError: (errorState: { hasError: boolean, message: string }) => void;
+  setError: (errorState: { hasError: boolean; message: string }) => void;
 }
 
-export const uploadFile = async ({ file, setUploadProgress, setImgUrls, setError }: uploadFileProps) => {
+export const uploadFile = async ({
+  file,
+  setUploadProgress,
+  setImgUrls,
+  setError,
+}: uploadFileProps) => {
   const formData = new FormData();
   formData.append("image", file);
   formData.append("key", "00dacc9c1e93806b67618c3a2ca36fb8");
