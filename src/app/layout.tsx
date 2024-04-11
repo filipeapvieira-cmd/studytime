8a1299ca2aef8dd0c155101bafd2a8530de28680
FILIPE VIEIRA <no@no.com>
2024-04-11 20:40:00 +0200

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/header/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Providers from "@/lib/Providers";
