2013984b481e95285fc28d2289911ee6af0b8541
FILIPE VIEIRA <no@no.com>
2024-04-14 19:33:24 +0200

    Add Footer component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Providers from "@/lib/Providers";
 import { Toaster } from "@/components/ui/toaster";
 import { Metadata } from "next";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background min-h-screen`}>
+      <body
+        className={`${inter.className} bg-background min-h-screen flex flex-col`}
+      >
         <Providers>
           <Header />
           {children}
         </Providers>
+        <Footer />
         <Toaster />
       </body>
     </html>
